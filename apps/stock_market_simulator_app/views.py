8dcebec578be53e503b0f5198b8de7d44b07a697
timlichen <tim.li.chen@gmail.com>
2017-04-18 14:28:26 -0400

    historical json data now on tempalate

--- apps/stock_market_simulator_app/views.py
+++ apps/stock_market_simulator_app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, redirect
-import ystockquote
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 from django.utils.safestring import SafeString
+import ystockquote
+import json
 from pprint import pprint
 from datetime import datetime
 
@@ -13,16 +15,23 @@ def posted_historic_stock_data(request):
 
     start = request.POST['start'].split("/")
     end = request.POST['end'].split("/")
+    symbol = request.POST['symbol']
 
     date_start = datetime(int(start[2]), int(start[0]), int(start[1])).strftime('%Y-%m-%d')
-    print date_start
+
 
     date_end = datetime(int(end[2]), int(end[0]), int(end[1])).strftime('%Y-%m-%d')
-    print date_end
-    
-    pprint(ystockquote.get_historical_prices('GOOGL', date_start, date_end))
+
+
+    historic_stock_data  = ystockquote.get_historical_prices(symbol, date_start, date_end)
+
+
+    print type(historic_stock_data)
+    # return historic_stock_data
+    # messages.info(request, historic_stock_data)
 
     # send data through reverse route, then display
     # set validations for date
-
-    return redirect("/")
+    context = { "historic_stock_data": SafeString(historic_stock_data)}
+    return render(request, 'stock_market_sim_templates/hist_results.html', context)
+    # return redirect(reverse('stock_market_sim_app:index'))
